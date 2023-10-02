ba828dffe0ec8addcedab119c8d71e71a66cffa1
Jibel <itsjibel@gmail.com>
2023-10-02 22:53:04 +0330

    starting the APIs implementation

--- server/src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('/sas')
-export class AppController {
-  @Get()
-  getUser() {
-    return 'I am a great person';
-  }
-}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,38 +1,122 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import * as express from 'express';
+import * as mysql from 'mysql2';
+import * as crypto from 'crypto';
+import * as jwt from 'jsonwebtoken';
 import * as fs from 'fs';
 import * as https from 'https';
 import * as cookieParser from 'cookie-parser';
+import * as multer from 'multer';
+import * as nodemailer from 'nodemailer';
+import * as cors from 'cors';
+import * as dotenv from 'dotenv';
 
-async function bootstrap() {
-  const app = express();
+const app = express();
+app.use(express.static('../public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+dotenv.config();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-  // Load SSL certificate and private key
-  const privateKey = fs.readFileSync('key.pem', 'utf8');
-  const certificate = fs.readFileSync('cert.pem', 'utf8');
+const jwtSecretKey = process.env.JWT_SECRET;
+const accessTokenExpiry = '15m';
+const refreshTokenExpiry = '15d';
 
-  const credentials = {
-    key: privateKey,
-    cert: certificate,
-    passphrase: process.env.CREDENTIALS_PASSPHRASE,
-  };
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 
-  // Create an HTTPS server
-  const httpsServer = https.createServer(credentials, app);
+app.post('/api/signup', (req, res) => {
+});
 
-  // Middleware
-  app.use(express.static('../public'));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(cookieParser());
+app.post('/api/login', (req, res) => {
+});
 
-  app.post('/auth/test', (req, res) => {
-    res.send('Test route works');
-  });
+function verifyRefreshToken(req, res, next) {
+  const refreshToken = req.headers.authorization?.split(' ')[1];
+  if (refreshToken) {
+      jwt.verify(refreshToken, jwtSecretKey, (err, decoded) => {
+          if (err) {
+              return res.status(403).json({ message: 'Invalid refresh token' });
+          }
+          req.user = decoded;
+          next();
+      });
+  } else {
+      return res.status(403).json({ message: 'Refresh token not provided' });
+  }
+}
 
-  const port = 443;
-  httpsServer.listen(port, () => {
-    console.log(`Server is running on port https://jchat.com`);
+app.get('/api/checkLoggedIn', verifyRefreshToken, (req, res) => {
+});
+
+function verifyAccessToken(req, res, next) {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) {
+      return res.status(403).json({ message: 'Access token not provided' });
+  }
+
+  jwt.verify(token, jwtSecretKey, (err, decoded) => {
+      if (err) {
+          return res.status(403).json({ message: 'Invalid access token' + token });
+      }
+
+      req.user = decoded; // Attach the user information to the request object
+      next(); // Continue to the next middleware or route handler
   });
 }
 
-bootstrap();
+app.get('/api/profile/:username', verifyAccessToken, (req, res) => {
+});
+
+app.post('/api/refreshAccessToken', (req, res) => {
+});
+
+const revokedTokens = [];
+
+app.post('/api/logout', (req, res) => {
+});
+
+app.post('/api/editProfile/:username', verifyAccessToken, upload.single('pfp'), (req, res) => {
+});
+
+app.post('/api/sendVerificationEmail', verifyAccessToken, (req, res) => {
+});
+
+app.get('/api/verifyUserEmail', (req, res) => {
+});
+
+app.post('/api/sendPasswordRecoveryEmail', (req, res) => {
+});
+
+app.get('/api/isValidRecoverPasswordToken', (req, res) => {
+});
+
+app.get('/api/recoverUserPassword', (req, res) => {
+});
+
+const privateKey = fs.readFileSync('key.pem', 'utf8');
+const certificate = fs.readFileSync('cert.pem', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  passphrase: process.env.CREDENTIALS_PASSPHRASE,
+};
+
+const httpsServer = https.createServer(credentials, app);
+const port = 443;
+
+// WebSocket and other server setup here
+
+httpsServer.listen(port, () => {
+  console.log(`Server is running on port https://jchat.com`);
+});
